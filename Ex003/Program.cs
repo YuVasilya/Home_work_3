7e0cbf94dd3b94e17eea6ca3cc043f26c503e59b
Vasilya <lady_drakula_94@mail.ru>
2022-09-28 21:56:07 +0500

    Ex003 работает, но нужно доработать

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,17 @@
 ﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Создай таблицу кубов");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+int[] array = new int[count];
+int i = 0;
+array[i] = 1;
+
+while (i < count)
+{
+    int numberCube = array[i] * array[i] * array[i];
+    Console.Write(numberCube + " ");
+    array[i + 1] = array[i] + 1;
+    i ++;
+}
+Console.WriteLine("end");
